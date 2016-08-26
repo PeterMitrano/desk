4da79f39615a7e22d9f321149e49fb5b7f395e2e
Peter Mitrano <mitranopeter@gmail.com>
2016-08-26 02:16:22 -0400

    fix pins

--- src/main.h
+++ src/main.h
@@ -1,3 +1,4 @@
+#include <Arduino.h>
 
 const char WiFiSSID[] = "Fruit453";
 const char WiFiPSK[] = "RoryAndrewPeter";
@@ -5,8 +6,8 @@ const char WiFiPSK[] = "RoryAndrewPeter";
 const float SIT = 1.0;
 const float STAND = 12.0;
 const int LED_PIN = 5; // Thing's onboard LED
-const int MOTOR_PIN = 1;
-const int HEIGHT_POT_PIN = 3;
+const int MOTOR_PIN = 2;
+const int HEIGHT_POT_PIN = A0;
 const float MIN_H = 0.0; // volts?
 const float MAX_H = 5.0; // volts?
 const float kP = 0.25; // if we're off by 20 bits (.25"), output 5 (small non-zero correction)
